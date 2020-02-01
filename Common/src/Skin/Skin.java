2a9cf008dca11c315a2b13feaa0b7e22f0b81a14
Vasantha krishnan <spec18krish@gmail.com>
2020-02-01 15:20:26 +1300

    common

---
+++ Common/src/Skin/Skin.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Skin;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+
+/**
+ *
+ * @author Vidhya Mohan
+ */
+public class Skin {
+    
+    public Color frameBackgroundColor = new Color(237, 240, 242);
+    public Color panelColor = new Color(62, 170, 209);
+    public Color panelLightCopyColor = new Color(224, 174, 119);
+    public Dimension frameDimension = new Dimension(2100, 1400);
+    public Color darkTextColor = new Color(65, 71, 85);
+    public Color darkButtonColor = new Color(42, 168, 159);
+    public Color buttonForeGroundColor = new Color(255, 255, 255);
+    
+    public Color skyGreenColor = new Color(23, 183, 127);
+    public Color blueGreenColor = new Color(42, 168, 159);
+    public Color slateBlackColor = new Color(71, 73, 76);
+    public Color skyBlueColor = new Color(62, 170, 209);
+    public Color silverGreyColor = new Color(237, 240, 242);
+    public Color darkSilverColor = new Color(224, 224, 224);
+    public Color milkWhiteColor = new Color(255,255,255);
+    public Color lightSkyBlueColor = new Color(0,255,255);
+    public Color lightSteelBlueColor = new Color(9,188,245);
+    public Color sandalYellowColor = new Color(243, 156,16);
+    public Color bloodRedColor = new Color(221 ,76 ,57);
+    public Color darkBlueColor = new Color(0,115,182);
+    public Color rasnaSafronColor = new Color(255,94,0);
+    
+    public Font font17 = new Font("SansSerif", Font.PLAIN, 17);
+    public Font font18 = new Font("SansSerif", Font.PLAIN, 18);
+    public Font font20 = new Font("SansSerif", Font.PLAIN, 20);
+    public Font font22 = new Font("SansSerif", Font.PLAIN, 22);
+    public Font font24 = new Font("SansSerif", Font.PLAIN, 24);
+    public Font font26 = new Font("SansSerif", Font.PLAIN, 26);
+    public Font fontBold18 = new Font("SansSerif", Font.BOLD, 18);
+    public Font fontBold20 = new Font("SansSerif", Font.BOLD, 20);
+    public Font fontBold24 = new Font("SansSerif", Font.BOLD, 24);
+    public Font fontBold28 = new Font("SansSerif", Font.BOLD, 28);
+    public Font fontBold30 = new Font("SansSerif", Font.BOLD, 30);
+    
+    public Dimension fieldDimension = new Dimension(300, 30);
+    
+}
